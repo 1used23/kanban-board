e43292f584fe9453dd0a16a7f2685d41c7800495
1used23 <diziwii@gmail.com>
2020-02-24 03:20:35 +0300

    Updates

---
+++ precache-manifest.a89f2602549ede528411bf1aaeed1477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcad32e048769a546685d59b4e95b6e9",
+    "url": "/kanban-board/index.html"
+  },
+  {
+    "revision": "358e306c3b5fed26fd69",
+    "url": "/kanban-board/static/css/main.a483c2af.chunk.css"
+  },
+  {
+    "revision": "ddafa02ef06119c14f8e",
+    "url": "/kanban-board/static/js/2.c75dbf1d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/kanban-board/static/js/2.c75dbf1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "358e306c3b5fed26fd69",
+    "url": "/kanban-board/static/js/main.abaffb69.chunk.js"
+  },
+  {
+    "revision": "b22ac035c307b5cc6bd3",
+    "url": "/kanban-board/static/js/runtime-main.bd470873.js"
+  }
+]);
